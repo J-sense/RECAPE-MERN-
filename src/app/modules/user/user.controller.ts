2aab6bd9ab635718aca260c09935334b8dd053ff
J-sense <jishan1873@gmail.com>
2025-05-01 21:44:16 +0600

    add higer order function

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,13 +1,14 @@
-import { Response, Request, NextFunction } from 'express';
+import { Response, Request, NextFunction, RequestHandler } from 'express';
 import { UserServices } from './user.services';
 import { sendResponse } from '../../utilis/sendResponse';
 import status from 'http-status';
-const createStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
+const catchAsync = (fn: RequestHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((error) => next(error));
+  };
+};
+const createStudent = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const { password, student } = req.body;
     const result = await UserServices.createStudent(password, student);
     sendResponse(res, {
@@ -16,10 +17,8 @@ const createStudent = async (
       message: 'Student created successfully',
       data: result,
     });
-  } catch (err) {
-    next(err);
-  }
-};
+  },
+);
 export const UserController = {
   createStudent,
 };
