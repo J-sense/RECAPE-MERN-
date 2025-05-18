dd23c39b9a463301c6961641006a9443bad130c4
J-sense <jishan1873@gmail.com>
2025-05-18 19:08:53 +0600

    handle zod error

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,30 +1,49 @@
 import { NextFunction, Request, Response } from 'express';
 import { ZodError } from 'zod';
-
+export type TerrorSource = {
+  path: string | null;
+  message: string;
+}[];
 const globalErrorHandler = (
   err: any,
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
+  const success = false;
   let statusCode = err.statusCode || 500;
   let message = err.message || 'Something went wrong';
-  type TerrorSource = {
-    path: string | null;
-    message: string;
-  }[];
-  const errorSource: TerrorSource = [
+
+  let errorSource: TerrorSource = [
     {
       path: '',
       message: 'Something went wrong',
     },
   ];
+  const handleZodError = (err: ZodError) => {
+    const errorSources = err.issues.map((issue) => {
+      return {
+        path: issue?.path[issue?.path.length - 1] as string,
+        message: issue?.message,
+      };
+    });
+    const statuscode = 400;
+    const message = 'Validation Error';
+    return {
+      statuscode,
+      message,
+      errorSources,
+    };
+  };
+
   if (err instanceof ZodError) {
-    statusCode: 400;
-    message: 'zod error ';
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError.statuscode;
+    message = simplifiedError.message;
+    errorSource = simplifiedError.errorSources;
   }
   res.status(statusCode).json({
-    success: false,
+    success,
     message,
     errorSource,
   });
