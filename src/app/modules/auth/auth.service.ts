7807d2741c55ce2fc74a597eeaf90d952a81ae55
J-sense <jishan1873@gmail.com>
2025-05-23 10:19:40 +0600

    jwt sign in

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -2,7 +2,8 @@ import status from 'http-status';
 import AppError from '../../error';
 import { User } from '../user/user.model';
 import bcrypt from 'bcrypt';
-
+import jwt from 'jsonwebtoken';
+import config from '../../config';
 type TLogin = {
   id: string;
   password: string;
@@ -15,24 +16,33 @@ const login = async (payload: TLogin) => {
     throw new AppError(status.NOT_FOUND, 'User does not exist');
   }
 
-  //   if (isUserExist.status !== 'in-progress') {
-  //     throw new AppError(status.BAD_REQUEST, 'User is blocked');
-  //   }
+  if (isUserExist.status !== 'in-progress') {
+    throw new AppError(status.BAD_REQUEST, 'User is blocked');
+  }
 
-  //   if (isUserExist.isDeleted === true) {
-  //     throw new AppError(status.BAD_REQUEST, 'User is deleted');
-  //   }
+  if (isUserExist.isDeleted === true) {
+    throw new AppError(status.BAD_REQUEST, 'User is deleted');
+  }
 
-  //   const isPasswordCompared = await bcrypt.compare(
-  //     payload.password,
-  //     isUserExist.password as string,
-  //   );
+  const isPasswordCompared = await bcrypt.compare(
+    payload.password,
+    isUserExist.password as string,
+  );
 
-  //   if (!isPasswordCompared) {
-  //     throw new AppError(status.UNAUTHORIZED, 'Password not matched');
-  //   }
-
-  return isUserExist;
+  if (!isPasswordCompared) {
+    throw new AppError(status.UNAUTHORIZED, 'Password not matched');
+  }
+  const jwtPayload = {
+    userId: isUserExist?.id,
+    userRole: isUserExist?.role,
+  };
+  const accessToken = jwt.sign({ jwtPayload }, config.jwt_access as string, {
+    expiresIn: '10d',
+  });
+  return {
+    accessToken,
+    needsPasswordChange: isUserExist?.needsPasswordChange,
+  };
 };
 
 export const authService = {
